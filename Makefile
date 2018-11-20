6ed13cdee102f274f5a7de1948cbceec388b6138
Marc Cataford <c.marcandre@gmail.com>
2018-11-20 12:29:54 -0500

    Test coverage java wrapper (#17)
    * test: coverage for Java wrapper

--- Makefile
+++ Makefile
@@ -5,3 +5,11 @@ build:
 .PHONY: package
 package:
 	mvn package -e
+
+.PHONY: test
+test:
+	mvn test
+
+.PHONY: clean_install
+clean_install:
+	mvn clean install
---
+++ src/test/java/io/jenkins/plugins/jenkinstl/MenuItemTest.java
@@ -0,0 +1,15 @@
+package io.jenkins.plugins.jenkinstl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MenuItemTest {
+    @Test
+    public void shouldComposeTargetUrlProperly() {
+        String mockUrl = "http://jenkinsbox.com/job/1";
+        MenuItem item = new MenuItem(mockUrl);
+        String composedUrl = item.getUrlName();
+        String expected = "/plugin/jenkins-timeline/index.html?build_url="+mockUrl;
+        Assert.assertEquals(composedUrl, expected);
+    }
+}
