0451d762d0084e74b86ec95f9a233101ac47f94a
Marc Cataford <marc.cataford@tophatmonocle.com>
2018-11-20 17:25:56 -0500

    feat: ported app to React

--- Makefile
+++ Makefile
@@ -1,6 +1,18 @@
+.PHONY: clean_webapp
+clean_webapp:
+	find ./src/main/webapp -not -name '*jenkins*' -delete
+
+.PHONY: build_all
+build_all:
+	make build_webapp && make build
+
 .PHONY: build
 build:
 	mvn install -e
+
+.PHONY: build_webapp
+build_webapp:
+	cd jenkinstl && PUBLIC_URL=/plugin/jenkins-timeline npm run build-to-plugin
 
 .PHONY: package
 package:
---
+++ jenkinstl/src/App.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import axios from 'axios'
+import Chart from 'react-google-charts'
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      stages: []
+    }
+  }
+
+  buildApiURL = url => {
+    return `${url}wfapi/describe`
+  }
+
+  componentDidMount() {
+    const buildUrl = window.location.href.split('?')[1].split('=')[1]
+    const buildEndpoint = this.buildApiURL(buildUrl)
+    axios
+      .get(buildEndpoint)
+      .then(result => {
+        const stages = result.data.stages
+        const promises = []
+        stages.forEach(stage => {
+          const stageLink = stage._links.self.href
+          promises.push(this.getStageInfo(stageLink))
+        })
+        Promise.all(promises).then(r => {
+          this.setState(
+            {
+              stages: r,
+              start: result.data.startTimeMillis,
+              duration: result.data.durationMillis,
+              end: result.data.startTimeMillis + result.data.durationMillis
+            }
+          )
+        })
+      })
+  }
+
+  getStageInfo = stageEndpoint => {
+    return axios.get(stageEndpoint)
+      .then(result => {
+        const stageData = result.data
+        const stage = {
+          title: stageData.name,
+          start: stageData.startTimeMillis,
+          steps: result.data.stageFlowNodes.map(node => {
+            return {
+              start: new Date(node.startTimeMillis),
+              end: new Date(node.startTimeMillis + node.durationMillis),
+              title: node.name,
+              status: node.status,
+              stage: stageData.name,
+            }
+          })
+        }
+        
+        return stage
+      })
+  }
+
+  formatChartRows = () => {
+    const data = [
+      [
+        { type: 'string', id: 'Stage' },
+        { type: 'string', id: 'Step' },
+        { type: 'date', id: 'Start' },
+        { type: 'date', id: 'End' },
+      ],
+    ]
+
+    this.state.stages.forEach(stage => {
+      stage.steps.forEach(step => {
+        data.push([
+          stage.title,
+          step.title,
+          step.start,
+          step.end
+        ])
+      })
+    })
+    return data
+  }
+
+
+  render() {
+    if (!this.state.stages) return null
+
+    return (
+      <Chart
+        width={`100%`}
+        chartType="Timeline"
+        loader={<div>Loading...</div>}
+        data={this.formatChartRows()}
+      />
+    )
+  }
+}
+
+
